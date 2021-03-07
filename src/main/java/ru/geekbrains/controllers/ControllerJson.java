4d3268d54717d163edb6bd5ab3b66a860edaccbc
Vitaly1785 <korsar1785@yandex.ru>
2021-03-07 19:22:24 +0300

    rename

--- src/main/java/ru/geekbrains/controllers/ControllerJson.java
+++ src/main/java/ru/geekbrains/controllers/ControllerJson.java
@@ -1,24 +1,21 @@
 package ru.geekbrains.controllers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.product.Product;
 
 
 @RestController
-public class HelloController2 {
+public class ControllerJson {
     @GetMapping(value = "/json/get", produces = "application/json")
-    @JsonFormat
     public Product getJson() {
         Product product = new Product();
         product.setId(1);
         product.setTitle("Phone");
         product.setCost(200.0);
-        ObjectMapper objectMapper = new JsonMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.convertValue(product, Product.class);
     }
 }
